61fdd749004663d7f54bc42e38b2e5cae29a4bd4
Kyle <user@userscomputer9.attlocal.net>
2017-05-07 15:26:36 -0500

    update port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express        = require('express');
 const bodyParser     = require('body-parser');
 const app            = express();
 
-const port = 8080;
+const port = process.env.PORT || 3000;
 require('./app/routes/')(app);
 app.listen(port, () => {
   console.log('We are live on ' + port);
